f9a818d80d49c21cfc293064d83aa65b582f5ef0
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-23 23:57:53 +0200

    feat: initializing view model for main page

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using wallpaper_manager.Models;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Pickers.Provider;
+using WinRT.Interop;
+
+namespace wallpaper_manager.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        // Importing DLL for desktop wallpaper
+}
