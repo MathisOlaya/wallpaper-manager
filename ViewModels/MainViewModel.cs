cd1b0d63e0347ecc7822e14e3bbf25a5cb8b5f3b
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-24 00:02:28 +0200

    feat: adding function for switching desktop wallpaper

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -67,4 +67,17 @@ namespace wallpaper_manager.ViewModels
             }
         }
 
+        // Method for updating desktop background
+        private void SetWallpaper(String path)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue(@"WallpaperStyle", 0.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+    }
+
+
 }
