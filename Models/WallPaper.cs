92c149bba4a106c050be49cdf358622844d6ff99
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-23 23:58:47 +0200

    feat(model): creating model for Wallpaper

---
+++ Models/WallPaper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wallpaper_manager.Models
+{
+    public class WallPaper
+    {
+        public required string ImagePath { get; set; }
+    }
+}
