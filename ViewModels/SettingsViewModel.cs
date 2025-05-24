f8e6a7f7b1d78f0007e5ae989d703e2a9c30c0fb
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-24 20:40:52 +0200

    feat: binding setting view model to page

---
+++ ViewModels/SettingsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wallpaper_manager.ViewModels
+{
+    public partial class SettingsViewModel : ObservableObject
+    {
+}
