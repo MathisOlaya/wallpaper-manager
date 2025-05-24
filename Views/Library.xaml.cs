39cd703d6ca0c22ca9ddd3d462acb8f898ca2d9b
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-24 20:02:29 +0200

    feat: requesting model view search function when user press enter on input

---
+++ Views/Library.xaml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace wallpaper_manager.Views;
+
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class Library : Page
+{
+    public Library()
+    {
+        InitializeComponent();
+    }
+
+    private async void TextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key == Windows.System.VirtualKey.Enter)
+        {
+            var text = TextBox.Text;
+
+            if (this.DataContext is ViewModels.LibraryViewModel viewModel)
+            {
+                await viewModel.Search(text);
+            }
+        }
+    }
+}
