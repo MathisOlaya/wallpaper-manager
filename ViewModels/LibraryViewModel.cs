4b53a888256f795bd7a022b58968dad28d8b8d10
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-24 20:01:16 +0200

    feat: create library model view

---
+++ ViewModels/LibraryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web;
+using wallpaper_manager.Models;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage;
+
+namespace wallpaper_manager.ViewModels
+{
+    public partial class LibraryViewModel : ObservableObject
+    {
+}
