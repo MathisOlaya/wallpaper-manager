8d9be56977bf49635e254069d09fbc3aa929e3fa
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-24 20:35:36 +0200

    feat: define model for pixabay http response

---
+++ Models/PixabayResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace wallpaper_manager.Models
+{
+    public class PixabayResponse
+    {
+        [JsonPropertyName("total")]
+        public int Total { get; set; }
+
+        [JsonPropertyName("totalHits")]
+        public int TotalHits { get; set; }
+
+        [JsonPropertyName("hits")]
+        public List<PixabayImage> Hits { get; set; }
+    }
+}
