110effb2806257248f43eb2c637c1e38427e1770
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-24 20:39:46 +0200

    feat: define all available languages for pixabay input

---
+++ Helpers/GlobalHelpers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wallpaper_manager.Helpers
+{
+    public static class GlobalHelpers
+    {
+        // Define ALL languages for API search
+        public static Dictionary<string, string> Languages = new Dictionary<string, string>
+        {
+            { "Čeština", "cs" },
+            { "Dansk", "da" },
+            { "Deutsch", "de" },
+            { "English", "en" },
+            { "Español", "es" },
+            { "Français", "fr" },
+            { "Bahasa Indonesia", "id" },
+            { "Italiano", "it" },
+            { "Magyar", "hu" },
+            { "Nederlands", "nl" },
+            { "Norsk", "no" },
+            { "Polski", "pl" },
+            { "Português", "pt" },
+            { "Română", "ro" },
+            { "Slovenčina", "sk" },
+            { "Suomi", "fi" },
+            { "Svenska", "sv" },
+            { "Türkçe", "tr" },
+            { "Tiếng Việt", "vi" },
+            { "ไทย", "th" },
+            { "Български", "bg" },
+            { "Русский", "ru" },
+            { "Ελληνικά", "el" },
+            { "日本語", "ja" },
+            { "한국어", "ko" },
+            { "中文", "zh" }
+        };
+    }
+}
